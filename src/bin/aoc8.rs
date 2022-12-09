7a9e139564b6fcade3bebbf01a1eb8f4b831d8aa
SLIMPANDA\janio <jani.peltonen@gmail.com>
2022-12-09 21:47:39 +0000

    aoc8 - part2

--- src/bin/aoc8.rs
+++ src/bin/aoc8.rs
@@ -7,6 +7,24 @@ fn update_scan( x: usize, y: usize, highest_seen: &mut i8, tree_mask: &mut Vec<V
     }
 }
 
+fn cast_ray( mut pos: (i32,i32), dir: (i32, i32), tree_map: &Vec<Vec<u8>> ) -> u32 {
+    let start_height = tree_map[pos.0 as usize][pos.1 as usize];
+    let mut visible_count = 0;
+    loop {
+        pos.0 += dir.0;
+        pos.1 += dir.1;
+        visible_count += 1;
+        if pos.0 == 0 || pos.1 == 0 || pos.0 == tree_map.len() as i32 -1 || pos.1 == tree_map.len() as i32 -1 {
+            break;
+        }
+        if tree_map[pos.0 as usize][pos.1 as usize] >= start_height {
+            break;
+        } 
+    }
+    return visible_count;
+}
+
+
 fn main() {
     let input = fs::read_to_string("inputs/aoc8.txt").unwrap();
     let lines = input.lines().collect::<Vec<&str>>(); 
@@ -37,4 +55,21 @@ fn main() {
         }
     }
     println!( "total visible: {}", total);
+
+    // Part 2
+    let mut best_score = 0;
+    for y in 1..height-1{
+        let mut highest_seen = [-1 as i8;4];
+        for x in 1..width-1 {
+            let start_pos = (y as i32, x as i32 );
+            let mut score =  cast_ray( start_pos, (0,-1), &tree_map );
+            score  *= cast_ray( start_pos, (0,1), &tree_map );
+            score  *= cast_ray( start_pos, (1,0), &tree_map );
+            score  *= cast_ray( start_pos, (-1,0), &tree_map );
+            if score > best_score {
+                best_score = score;
+            }
+        }
+    }
+    println!( "Best score: {}", best_score);
 }
